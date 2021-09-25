db0cc74a26d7f2c65e013b77e39f61d4b3792536
yash <yashsngh57@gmail.com>
2021-09-25 17:40:03 +0530

    edit page added

--- src/AddItem.js
+++ src/AddItem.js
@@ -1,12 +1,29 @@
 import React from 'react'
 import { useParams } from "react-router-dom";
+import AddItemPage from './AddItemPage';
+import useFetch from './useFetch';
 function AddItem() {
     const { id } = useParams();
-    return (
-        <div>
-            <p>{id}</p>
-        </div>
-    )
+    const { data: data, error, isPending } = useFetch('http://localhost:8000/items/' + id);
+   
+
+        return (
+           <div>
+                     { data && (
+      <>
+        <h2>{ data.name }</h2>
+       <img src={data.src} alt =""/>
+       
+      </>
+     
+    )}
+           
+           </div>
+           
+        );
+
+    
+
 }
 
 export default AddItem
---
+++ src/AddItemPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function AddItemPage({name,src,id,quantity}) {
+    return (
+        <div>
+            <p>{name}</p>
+        </div>
+    )
+}
+
+export default AddItemPage
