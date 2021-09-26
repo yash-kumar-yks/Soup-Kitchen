5f9e436823a1dc99a61c83df94cd14bff6de55cd
yash <yashsngh57@gmail.com>
2021-09-26 20:19:27 +0530

    add inventory page

--- src/AddItem.js
+++ src/AddItem.js
@@ -1,29 +1,56 @@
 import React from 'react'
-
+import { useState } from "react";
 function AddItem() {
-    return (
-        <div>
-        <div className= "labels">
-        <label>
-            Name:
-            <input type="text" name="name"  />
-        </label>
-        <label>
-            Image-URL:
-            <input type="text" name="name"  />
-        </label>
-        <label>
-           ID
-            <input type="text" name="name" />
-        </label>
-        <label>
-            Amount:
-            <input type="text" name="name"  />
-        </label>
-        </div>
-        <button type="submit"> POST..</button>
-        </div>
-    )
+    
+  const [name, setName] = useState('');
+  const [src, setSrc] = useState('');
+  const [quantity, setQuantity] = useState(0);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const data = { name, src, quantity };
+
+    fetch('http://localhost:8000/items/', {
+      method: 'POST',
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data)
+    }).then(() => {
+      console.log('new item added');
+    })
+  }
+
+  return (
+    <div className="create">
+      <h2>Add a New Item</h2>
+      <form onSubmit={handleSubmit}>
+        <label>Item Name:</label>
+        <input 
+          type="text" 
+          required 
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <label>Image URL:</label>
+        <input 
+          type="text" 
+          required 
+          value={src}
+          onChange={(e) => setSrc(e.target.value)}
+        />
+        <label>Amount:</label>
+        <input 
+          type="text" 
+          required 
+          value={quantity}
+          onChange={(e) => setQuantity(e.target.value)}
+        />
+       
+        <button>Add Item</button>
+      </form>
+    </div>
+  );
 }
+ 
+
 
 export default AddItem
